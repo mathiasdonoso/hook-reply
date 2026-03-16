9ca3e0160fb972297d8a1d7ba218ddec8df8101e
Mathias <mathiasd88@gmail.com>
2026-03-15 23:12:14 -0300

    feat: basic flag validation for serve command

--- internal/cli/registry.go
+++ internal/cli/registry.go
@@ -12,12 +12,14 @@ type CommandNode struct {
 	Setup       func(fs *flag.FlagSet) func() error
 }
 
+const DefaultPort = 3000
+
 var CommandsRegistry = map[string]*CommandNode{
 	"serve": {
 		Name:        "serve",
 		Description: "Creates a proxy server that intercepts and forwards webhook requests.",
 		Setup: func(fs *flag.FlagSet) func() error {
-			port := fs.Uint("port", 0, "Port where the webhook server listens for incoming requests.")
+			port := fs.Uint("port", DefaultPort, "Port where the webhook server listens for incoming requests.")
 			forward := fs.String("forward", "", "URL to forward captured webhook requests to.")
 
 			return func() error {
